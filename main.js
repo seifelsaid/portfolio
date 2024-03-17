91917814e4894a684d33c1d422c23f0c4886a370
seif_Elsayed <saifelsaid73@gmail.com>
2024-03-17 23:08:37 +0200

    upload website

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(" .multi-text",{
+    strings:['Frontend Devoloper','Fullsatck Devoloper','Software Engineer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
